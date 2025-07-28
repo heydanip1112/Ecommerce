d92e1d43e0373b799c57571aa9eaaa6d2843965f
heydanip1112 <163697816+heydanip1112@users.noreply.github.com>
2025-07-28 17:26:56 -0600

    Shopping cart y sign in

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -21,6 +21,18 @@ const Header = () => {
             {APP_NAME}
           </span>
         </Link>
+        <div className='space-x-2'>
+            <Button asChild variant='ghost'>
+                <Link href='/cart'>
+                    <ShoppingCart /> Cart
+                </Link>
+            </Button>
+            <Button asChild variant='ghost'>
+                <Link href='/sign-in'>
+                    <UserIcon /> Sign In
+                </Link>
+            </Button>
+        </div>
       </div>
     </header>
   );
