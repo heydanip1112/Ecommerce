b35d096cf57f652ae8b9543e4e9350ee4e746a6a
heydanip1112 <163697816+heydanip1112@users.noreply.github.com>
2025-07-29 11:03:07 -0600

    schemas

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -6,7 +6,7 @@ import { Product } from "@/types";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <Card className="w-full max-w-sm flex flex-col">
+    <Card className="w-full max-w-sm flex flex-col border-0 shadow-sml">
       <CardHeader className="p-0 items-center">
         <Link href={`/product/${product.slug}`}>
           <Image
@@ -20,20 +20,22 @@ const ProductCard = ({ product }: { product: Product }) => {
         </Link>
       </CardHeader>
 
-      <CardContent className="p-4 flex flex-col gap-2 flex-1">
+      <CardContent className="p-2 flex flex-col gap-1 flex-1">
         <div className="text-xs text-gray-500">{product.brand}</div>
         <Link href={`/product/${product.slug}`}>
-          <h2 className="text-sm font-medium">{product.name}</h2>
+          <h2 className="text-sm font-medium line-clamp-2 h-10">{product.name}</h2>
         </Link>
-        <p className="text-sm">{product.rating} Stars</p>
 
-        {/* Sección inferior */}
-        <div className="flex justify-between items-center mt-auto">
-          <ProductPrice value={Number(product.price)}/>
-          {product.stock <= 0 && (
-            <p className="text-sm text-red-600 font-medium">Out Of Stock</p>
-          )}
+        {/* Rating y precio alineados */}
+        <div className="flex items-center leading-none">
+            {product.stock > 0 ? (
+              <ProductPrice value={Number(product.price)} />
+            ) : (
+              <span className="text-xs text-red-600 font-medium">Out Of Stock</span> // Texto más pequeño
+            )}
         </div>
+
+  
       </CardContent>
     </Card>
   );
