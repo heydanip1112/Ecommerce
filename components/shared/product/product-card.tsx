5207a70b5cabeaa8efb65905f2f7b001b01417a7
heydanip1112 <163697816+heydanip1112@users.noreply.github.com>
2025-07-29 11:12:00 -0600

    cards

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,12 +1,17 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Product } from "@prisma/client";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import Link from "next/link";
 import Image from "next/image";
 import ProductPrice from "./product-price";
-import { Product } from "@/types";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <Card className="w-full max-w-sm flex flex-col border-0 shadow-sml">
+    <Card className="w-full max-w-sm flex flex-col border-0 shadow-sm">
       <CardHeader className="p-0 items-center">
         <Link href={`/product/${product.slug}`}>
           <Image
@@ -23,19 +28,25 @@ const ProductCard = ({ product }: { product: Product }) => {
       <CardContent className="p-2 flex flex-col gap-1 flex-1">
         <div className="text-xs text-gray-500">{product.brand}</div>
         <Link href={`/product/${product.slug}`}>
-          <h2 className="text-sm font-medium line-clamp-2 h-10">{product.name}</h2>
+          <h2 className="text-sm font-medium line-clamp-2 h-10">
+            {product.name}
+          </h2>
         </Link>
 
-        {/* Rating y precio alineados */}
-        <div className="flex items-center leading-none">
+        <div className="flex justify-between items-center text-sm h-6">
+          <div className="flex items-center gap-1">
+            <span>{Number(product.rating).toFixed(1)}</span>
+            <span>Stars</span>
+          </div>
+
+          <div className="flex items-center leading-none">
             {product.stock > 0 ? (
               <ProductPrice value={Number(product.price)} />
             ) : (
               <span className="text-xs text-red-600 font-medium">Out Of Stock</span> // Texto más pequeño
             )}
+          </div>
         </div>
-
-  
       </CardContent>
     </Card>
   );
